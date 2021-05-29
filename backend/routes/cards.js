8abe919d81cf8bb2232e1c8cbe017a803696f0ef
Surovev <dmitriysurovev@gmail.com>
2021-05-29 14:07:52 +0300

    cards joi fix

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -12,7 +12,7 @@ const {
 router.get('/', getCards);
 router.post('/', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required(),
+    name: Joi.string().required().min(2).max(30),
     link: Joi.string().uri().required(),
   }),
 }), createCard);
